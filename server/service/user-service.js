3ea3e12e8019c060f916a1b4567b356c2617948a
Dmitry Mumrenkov <dimony4519@gmail.com>
2022-02-26 21:36:13 +0300

    end registration

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,10 +1,12 @@
 const UserModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
 const uuid = require("uuid");
+const tokenService = require("./token-service");
 const MailService = require("./mail-service");
+const UserDto = require("../dtos/user-dto");
 
 class UserService {
-  async regictration(email, password) {
+  async registration(email, password) {
     const candidate = await UserModel.findOne({email});
     if (candidate) {
       throw new Error(`User with this email (${email}) already exist`);
@@ -12,7 +14,13 @@ class UserService {
     const hashPassword = await bcrypt(password, 3);
     const activationLink = uuid.v4();
     const user = await UserModel.create({email, password: hashPassword});
-    await MailService.sendActivationMail(email, activationLink)
+    await MailService.sendActivationMail(email, activationLink);
+
+    const userDto = new UserDto(user); // id, isActivated, email
+    const tokens = tokenService.generateTokens({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {...tokens, user: userDto};
   }
 }
 
